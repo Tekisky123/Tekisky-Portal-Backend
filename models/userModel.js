8a4e2a3d35934a2b7d6030866cbb0ed5cee54aa8
SadiyaFatema <ssumairakhan247@gmail.com>
2024-01-24 17:08:01 +0530

    update

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema=new mongoose.Schema({
      userName:{type:String,required:true,trim:true},
-     mobileNumber:{type:Number,min:10,required:true, unique:true},
+     mobileNumber:{type:String,min:10,required:true, unique:true},
      email:{type:String,required:true,unique:true},
      password:{type:String,required:true},
      userType:{type:String,required:true}
